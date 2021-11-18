1292004d562b567eed6374f1187c32947fa1db25
Fredrik Johansen <fredrik1206@gmail.com>
2021-11-18 18:22:56 +0100

    first commit

---
+++ LanguageSupport/Language.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LanguageSupport {
+	public class Language {
+
+		private string selectedLanguage;
+		private JsonDocument languageFormattedFile;
+
+		public Language() {
+			this.selectedLanguage = CultureInfo.CurrentCulture.Name;
+			string fileContent = this.getFileContent(this.selectedLanguage);
+
+			this.languageFormattedFile = this.formatFileContent(fileContent);
+		}
+
+		public string getLanguage() {
+			return this.selectedLanguage;
+		}
+
+		private string getFileContent(string countryCode) {
+			var assembly = Assembly.GetExecutingAssembly();
+
+			using (Stream stream = assembly.GetManifestResourceStream("LanguageSupport.Language." + countryCode + ".json"))
+			using (StreamReader reader = new StreamReader(stream)) {
+				string result = reader.ReadToEnd();
+
+				return result;
+			}
+		}
+
+		private JsonDocument formatFileContent(string fileContent) {
+			JsonDocument doc = JsonDocument.Parse(fileContent);
+
+			return doc;
+		}
+	}
+}
---
+++ LanguageSupport/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LanguageSupport {
+	internal class Program {
+		static void Main(string[] args) {
+			Language lang = new Language();
+
+			//string[] resources = lang.getResources();
+
+			//foreach (string item in resources) {
+			//	Console.WriteLine(item);
+			//}
+
+			Console.WriteLine(lang.getLanguage());
+		}
+	}
+}
